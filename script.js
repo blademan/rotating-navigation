f932a8ec759b334c03c715966a2dafbf984e93b8
Eduards Leimanis <blademan82@gmail.com>
2021-06-26 01:25:58 +0300

    add functionality with js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const buttonOpen = document.querySelector("#open");
+const buttonClose = document.querySelector("#close");
+const container = document.querySelector(".container");
+
+buttonOpen.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
+buttonClose.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
